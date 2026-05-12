4eab5eec76ce133d064aa333efcba821503d3fc9
Ostrastr21 <oscar.trujillo@udea.edu.co>
2026-05-11 23:07:54 -0500

    agregué QGraphicsScene y QGraphicsView a la ventana

--- src/juego.cpp
+++ src/juego.cpp
@@ -1,8 +1,23 @@
 #include "juego.h"
+#include <QPainter>
 
 Juego::Juego(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Tejo Cósmico");
     resize(800, 600);
+
+    // Crear la escena (el espacio donde viven los objetos del juego)
+    escena = new QGraphicsScene(this);
+    escena->setSceneRect(0, 0, 800, 600);
+    escena->setBackgroundBrush(QColor(20, 20, 50));  // azul oscuro, como el espacio
+
+    // Crear la vista (la cámara que muestra la escena)
+    vista = new QGraphicsView(escena, this);
+    vista->setRenderHint(QPainter::Antialiasing);
+    vista->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    vista->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    // Hacer que la vista ocupe toda la ventana
+    setCentralWidget(vista);
 }
 
 Juego::~Juego() {}
--- src/juego.h
+++ src/juego.h
@@ -2,12 +2,18 @@
 #define JUEGO_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
 class Juego : public QMainWindow {
     Q_OBJECT
 public:
     Juego(QWidget *parent = nullptr);
     ~Juego();
+
+private:
+    QGraphicsScene *escena;
+    QGraphicsView *vista;
 };
 
 #endif // JUEGO_H
